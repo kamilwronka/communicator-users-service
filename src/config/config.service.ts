6531160dccae820bb571cb7ce508a3bc7851303d
Kamil <kamilwronka7@gmail.com>
2022-05-10 09:49:26 +0200

    Updated config and pipeline

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,6 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+import { retrieveSecret } from '@communicator/common';
 
 const client = new SecretManagerServiceClient();
 dotenv.config();
@@ -26,14 +27,9 @@ class ConfigService {
   }
 
   public async retrieveSecrets() {
-    const [versions] = await client.listSecretVersions({
-      parent: 'projects/928190670092/secrets/communicator-dev-users-service',
-    });
-
-    const secret = await client.accessSecretVersion({
-      name: versions[0].name,
-    });
-    const data = JSON.parse(secret[0].payload.data.toString()) as TConfig;
+    const data = await retrieveSecret<TConfig>(
+      'projects/464228375192/secrets/communicator-dev-users',
+    );
 
     this.config = data;
   }
